7ba288b0d5f67b2fe0b7d122b124d693d4a4605f
Shah0912 <adi.shah0912@gmail.com>
2021-05-04 20:46:22 +0530

    Initial commit

---
+++ flappyBird.py
@@ -0,0 +1,14 @@
+import pygame
+import neat
+import os
+import time
+import random
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 800
+
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+
